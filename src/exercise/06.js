c4e097cb94108af4fd195f1d8e7eb0c7346df431
Hailey Hansard <haileyhansard@gmail.com>
2021-03-10 12:17:43 -0800

    Adds validation and error state if user types Uppercase into input

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,22 +4,30 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const usernameInputRef = React.useRef()
+  const [error, setError] = React.useState(null)
+
   function handleSubmit(event) {
     event.preventDefault()
-    const value = usernameInputRef.current.value
-    onSubmitUsername(value)
-    console.dir(event.target)
+    onSubmitUsername(event.target.elements.usernameInput.value)
+    //console.dir(event.target)
+  }
 
-  } 
+  function handleChange(event) {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lower case')
+  }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input ref={usernameInputRef} id="usernameInput" type="text" />
+        <input id="usernameInput" type="text" onChange={handleChange} />
       </div>
-      <button type="submit">Submit</button>
+      <div style={{color: 'red'}}>{error}</div>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
